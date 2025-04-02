52bd604aa592f51a59ea2c28cd7236d76aa13b74
Tendo33 <sjf1998112@gmail.com>
2025-04-02 15:06:26 +0800

    feat: update meta tags and remove unused components

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,11 +9,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     GlobalHeader: typeof import('./components/GlobalHeader.vue')['default']
-    README: typeof import('./components/README.md')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheCounter: typeof import('./components/TheCounter.vue')['default']
-    TheFooter: typeof import('./components/TheFooter.vue')['default']
-    TheInput: typeof import('./components/TheInput.vue')['default']
   }
 }
