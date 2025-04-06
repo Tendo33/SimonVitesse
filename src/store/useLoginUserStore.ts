e9323a6602d2c49916f1a0747bb33a917faf4b72
Tendo33 <sjf1998112@gmail.com>
2025-04-06 18:31:43 +0800

    add store

--- src/store/useLoginUserStore.ts
+++ src/store/useLoginUserStore.ts
@@ -1,16 +1,21 @@
 import { defineStore } from "pinia";
-import { userLogin, getCurrentUser } from "@/api/user";
+import { getCurrentUser } from "@/api/user";
 
 export const useLoginUserStore = defineStore("loginUser", () => {
 	const loginUser = ref<any>({
-        username: "",
-        password: "",
-    });
-	
-    async function fetchLoginUser(username: string, password: string) {
-        const res = await getCurrentUser();
-        if (res.data.code === 0 && res.data.data) {
-            loginUser.value = res.data.data;
-        }
-    }
+		username: "",
+		password: "",
+	});
+
+	async function fetchLoginUser(username: string, password: string) {
+		const res = await getCurrentUser();
+		if (res.data.code === 0 && res.data.data) {
+			loginUser.value = res.data.data;
+		}
+	}
+	function setLoginUser(newLoginUser: any): void {
+		loginUser.value = newLoginUser;
+	}
+
+	return { loginUser, fetchLoginUser, setLoginUser };
 });
