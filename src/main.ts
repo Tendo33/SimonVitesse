2a54e8b609d820152ef1389a9c0d806d3b646d4c
Tendo33 <sjf1998112@gmail.com>
2025-04-06 17:24:35 +0800

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { createApp } from "vue";
 //import './style.css'
 import App from "@/App.vue";
+import { createPinia } from "pinia";
 import Antd from "ant-design-vue";
 import "ant-design-vue/dist/reset.css";
 import router from "@/router";
+
 const app = createApp(App);
+const pinia = createPinia();
+
 app.use(Antd);
+app.use(pinia);
 app.use(router); // 挂载路由实例
 app.mount("#app");
